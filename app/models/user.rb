704416ff74081a525ffc9b3f81c0eb46b574ebf7
Louis S <schlomaham@gmail.com>
2016-09-17 22:02:00 +0000

    Add cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,18 +19,23 @@ class User < ApplicationRecord
   
    # Returns a random token
     def new_token
-     SecureRandom.urlsafe_base_64
+     SecureRandom.urlsafe_base64
     end
   end
   
-  # Remembers a user in the database for use in persistant sessions
+  # Remembers a user in the database for use in persistent sessions.
   def remember
-    self.rmember_token = User.new_token
+    self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
   
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
